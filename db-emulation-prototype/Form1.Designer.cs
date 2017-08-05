742bf087a648791019999b26aab769d32ed21f71
Jacob Young <jeyj13@gmail.com>
2017-08-05 10:49:37 -0400

    Initial Commit - incomplete prototype

---
+++ db-emulation-prototype/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace db_emulation_prototype
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtInfo1 = new System.Windows.Forms.TextBox();
+            this.lstOut = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(116, 237);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(438, 237);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(71, 52);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 2;
+            // 
+            // txtInfo1
+            // 
+            this.txtInfo1.Location = new System.Drawing.Point(71, 78);
+            this.txtInfo1.Multiline = true;
+            this.txtInfo1.Name = "txtInfo1";
+            this.txtInfo1.Size = new System.Drawing.Size(177, 106);
+            this.txtInfo1.TabIndex = 3;
+            // 
+            // lstOut
+            // 
+            this.lstOut.FormattingEnabled = true;
+            this.lstOut.Location = new System.Drawing.Point(363, 40);
+            this.lstOut.Name = "lstOut";
+            this.lstOut.Size = new System.Drawing.Size(218, 173);
+            this.lstOut.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(654, 364);
+            this.Controls.Add(this.lstOut);
+            this.Controls.Add(this.txtInfo1);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnSave);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtInfo1;
+        private System.Windows.Forms.ListBox lstOut;
+    }
+}
+
---
+++ db-emulation-prototype/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace db_emulation_prototype
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string[] savelines = new string[3];
+            savelines[1] = txtName.Text;
+            savelines[2] = txtInfo1.Text;
+            save_data(savelines);
+
+        }
+
+
+
+        public void save_data(string [] lines)
+        {
+            string[] lineholder = new string[255];
+            string linein;
+            int counter = 0;
+            bool isNum;
+            int n;
+            // Read the file and display it line by line.
+            System.IO.StreamReader filein =
+               new System.IO.StreamReader("c:/users/jey/documents/visual studio 2015/Projects/db-emulation-prototype/db-emulation-prototype/db.txt");
+            while ((linein = filein.ReadLine()) != null)
+            {
+                lineholder[counter] = linein;
+                counter++;
+            }
+            for(int o =0; o < counter; o++)
+            {
+                if ((isNum = int.TryParse(lineholder[o], out n)) == true)
+                {
+                    lines[0] = (n + 1).ToString();
+                }
+            }
+            filein.Close();
+            using (System.IO.StreamWriter fileout =
+        new System.IO.StreamWriter("c:/users/jey/documents/visual studio 2015/Projects/db-emulation-prototype/db-emulation-prototype/db.txt"))
+            {
+
+                foreach (string line in lines)
+                {
+
+                    {
+                        fileout.WriteLine(line);
+                    }
+                }
+                fileout.Close();
+            }
+        }
+    }
+
+}
